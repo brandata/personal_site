b2bb8629e7f29444677a73f8d0681867179bcbd9
Brandon <brandon.haw@gmail.com>
2018-08-20 22:26:27 -0400

    Updated Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => (
       <h3>
         I am a software developer based out of Toronto, Ontario, with a strong
         interest in <span>Machine Learning</span> and <span>Artificial Intelligence</span>.
-        On this page you'll find information about myself, my interests, and projects that I have worked on.
+        On this page you'll find information about myself, my interests, and projects that I have worked on. Use the social links below if you would like to get in touch with me!
 
       </h3>
       <hr />
